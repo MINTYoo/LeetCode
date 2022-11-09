bd2709450ff7d4ea1ad73145f8c97cb4ec507276
MINTYoo <107981047+MINTYoo@users.noreply.github.com>
2022-11-09 00:38:01 -0800

    Update Product_of_array_except_itself.cpp

--- Product_of_array_except_itself.cpp
+++ Product_of_array_except_itself.cpp
@@ -5,9 +5,11 @@ public:
         int* right = new int [nums.size()];
         left[0] = 1;
         right[nums.size()-1] = 1;
+        
         for(int i = 1; i< nums.size(); i++){
             left[i] = left[i-1] * nums[i-1]; 
         }
+        
         for(int i = nums.size()-2; i >=0; i--){
             right[i] = right[i+1] * nums[i+1]; 
         }
@@ -15,6 +17,7 @@ public:
         for(int i=0; i< nums.size(); i++){
             nums[i] = left[i] * right[i];
         }
+        
         return nums;
     }
 };
